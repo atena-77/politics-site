d0981d0b71b30fde89428571f3b8772ddb1747b9
atena-77 <ktomoya77@icloud.com>
2025-09-09 21:17:12 +0900

    Update estat.js

--- API/estat.js
+++ API/estat.js
@@ -1,4 +1,3 @@
 export default async function handler(req, res) {
-  const hasKey = Boolean(process.env.ESTAT_APP_ID);
-  return res.status(200).json({ ok: true, hasKey });
+  res.status(200).json({ ok: true, hasKey: Boolean(process.env.ESTAT_APP_ID) });
 }
